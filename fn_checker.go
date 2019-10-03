3ea0031213cb9cadb81282c9a23ab0e542265a25
Mahbub Zulkarnain <prof.bub@live.com>
2019-10-04 00:10:56 +0700

    Update fn_checker.go

--- fn_checker.go
+++ fn_checker.go
@@ -4,7 +4,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// Check for fixing "Error return value of '' is not checked (errcheck)"
+// FNChecker for fixing "Error return value of '' is not checked (errcheck)"
 func FNChecker(fn func() error) {
 	if err := fn(); err != nil {
 		log.Error(err)
